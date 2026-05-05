142513d737550448cfa3b12579f6109b02007a7d
nxiio <w.orellana@duocuc.cl>
2026-05-05 09:18:06 -0400

    se inicio el microservicio de seguimiento social

---
+++ social-ms/src/main/java/cl/worellana/social_ms/SocialMsApplication.java
@@ -0,0 +1,13 @@
+package cl.worellana.social_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMsApplication.class, args);
+	}
+
+}
---
+++ social-ms/src/test/java/cl/worellana/social_ms/SocialMsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.worellana.social_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
